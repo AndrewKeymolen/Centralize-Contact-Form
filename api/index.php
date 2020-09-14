55989b644a3c4397598cf2e027839b30001ee51a
Andrew Keymolen <andrewkeymolen@gmail.com>
2020-09-14 17:08:34 +0200

    headers

--- api/index.php
+++ api/index.php
@@ -3,7 +3,7 @@
 // require_once '<PATH TO>/sendgrid-php.php';
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, GET, OPTION");
-header("Access-Control-Allow-Headers: content-type");
+header("Access-Control-Allow-Headers: Content-Type");
 $rest_json = file_get_contents("php://input");
 $_POST = json_decode($rest_json, true);
 use SendGrid\Mail\Mail;
