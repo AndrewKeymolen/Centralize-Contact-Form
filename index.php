e14ed8095cfc6801c0eed8fb37ae5a342b91e353
Andrew Keymolen <andrewkeymolen@gmail.com>
2020-09-14 18:47:16 +0200

    Final touches

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $SendMailEmptyerrorMessage = "The email adress fiel is empty!";
 $SendMailInvaliderrorMessage = "The provided email is invalid!";
 $SendSubjectEmptyerrorMessage = "The subject field is empty!";
 $SendMessageEmptyerrorMessage = "The message field is empty!";
-$SendMailSuccessMessage = "Your message has been sent successfully! You should also have received a copy. Please check your SPAMS folder if you can't find it.";
+$SendMailSuccessMessage = "Your message has been sent successfully! You should also have received a copy. Please check your SPAM folder if you can't find it.";
 
 //Headers
 header("Access-Control-Allow-Origin: *");
