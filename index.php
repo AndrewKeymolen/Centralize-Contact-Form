0b9257bed2924ea0f8e5ae69c1df228ec725ea57
Andrew Keymolen <andrewkeymolen@gmail.com>
2020-09-14 17:40:46 +0200

    test

--- index.php
+++ index.php
@@ -16,8 +16,26 @@ $_POST = json_decode($rest_json, true);
 
 require 'vendor/autoload.php';
 
-$emailsg = new \SendGrid\Mail\Mail();
-
+$email = new \SendGrid\Mail\Mail();
+$email->setFrom("keymolenandrew@gmail.com", "Example User");
+$email->setSubject("Sending with SendGrid is Fun");
+$email->addTo("andrewkeymolen@gmail.com", "Example User");
+$email->addContent(
+    "text/plain", "and easy to do anywhere, even with PHP"
+);
+$email->addContent(
+    "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
+);
+$sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
+try {
+    $response = $sendgrid->send($email);
+    print $response->statusCode() . "\n";
+    print_r($response->headers());
+    print $response->body() . "\n";
+} catch (Exception $e) {
+    echo 'Caught exception: ',  $e->getMessage(), "\n";
+}
+/*
 if( empty($_POST['name'])) {
   echo json_encode(
     [
@@ -224,5 +242,5 @@ class Sender
     var_dump("End second mail");
   }
 
-}
+}*/
 ?>
